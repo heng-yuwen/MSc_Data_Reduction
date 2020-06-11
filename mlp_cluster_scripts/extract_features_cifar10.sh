252eda7669ada3d6a28a348e68a20645638ba474
Yuwen Heng <hengyuwen_alex@163.com>
2020-06-11 08:35:43 +0800

    Add mlp cluster bash script

---
+++ mlp_cluster_scripts/extract_features_cifar10.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=Teach-Standard
+#SBATCH --gres=gpu:4
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=0-08:00:00
+
+
+export CUDA_HOME=/opt/cuda-10.1.168_418_67/
+
+export CUDNN_HOME=/opt/cuDNN-7.6.0.64_9.2/
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}/
+
+mkdir -p ${TMP}/datasets/
+export DATASET_DIR=${TMP}/datasets/
+# Activate the relevant virtual environment:
+
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
+cd ..
+python extract_features_for_cifar10.py
