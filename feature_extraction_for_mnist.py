01413a106744f9ada3e09b95893d45e99152f3ec
Yuwen Heng <hengyuwen_alex@163.com>
2020-06-12 11:42:43 +0800

    Train mnist classifier with learning rate 0.001

--- feature_extraction_for_mnist.py
+++ feature_extraction_for_mnist.py
@@ -50,20 +50,20 @@ print("There are {} training samples and {} validation samples".format(x_train.s
 print("There are {} test samples.".format(x_test.shape[0]))
 
 # extract features
-features_train = model.extract(x_train, batch_size=batch_size)
-print("The shape of the extracted training sample features is: ", features_train.shape)
+# features_train = model.extract(x_train, batch_size=batch_size)
+# print("The shape of the extracted training sample features is: ", features_train.shape)
 
 # save features
 # model.save_features()
 
 # load features
-# model.load_features()
-# model.load_extractor()
-# model.load_classifier()
+model.load_features()
+model.load_extractor()
+model.load_classifier()
 
 # use dense layer to test feature quality
 history = model.train_classifier(y_train, epochs=100, batch_size=batch_size, validation_data=(x_valid, y_valid))
-model.save_history(history, name="train_classifier")
+model.save_history(history, name="train_classifier_2")
 
 # save trained model
 model.save_classifier()
--- lib/feature_extractor.py
+++ lib/feature_extractor.py
@@ -247,7 +247,7 @@ class NASNetLargeExtractor(FeatureExtractor):
             self.compressor_layer,
             self.classifier_layer
         ])
-        self.classifier.compile(optimizer=tf.keras.optimizers.SGD(0.01),
+        self.classifier.compile(optimizer=tf.keras.optimizers.SGD(0.001),
                                 loss='categorical_crossentropy',
                                 metrics=['accuracy'])
 
