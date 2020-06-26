d4cd6338871b6ff10673be6fa81600509e11f36c
Yuwen Heng <hengyuwen_alex@163.com>
2020-06-26 12:33:07 +0800

    Add comments.

--- lib/algorithms.py
+++ lib/algorithms.py
@@ -14,6 +14,10 @@ class EGDIS(object):
     Applications, 2020, 149: 113297. """
 
     def __init__(self, k=4):
+        """Init a new EGDIS instance
+        :param k: the nearest neighbor parameter, should be k+1 if the instance is in the dataset.
+        """
+
         self.k = k
         self.x = None
         self.y = None
@@ -22,17 +26,32 @@ class EGDIS(object):
         self.neigh = NearestNeighbors(n_neighbors=k, n_jobs=-1)
 
     def _irrelevance(self, x_kneighbors):
+        """Count the number of samples with different labels around x
+        :param x_kneighbors: the array of neighbor indices.
+        """
+
         score = 0
         for inx in range(1, self.k):
             if self.y[x_kneighbors[0]] != self.y[x_kneighbors[inx]]:
                 score += 1
         return score
 
-    def _densest(self, unique_x, size):
+    def _dense(self, unique_x, size):
+        """Calculate the dense score for sample x. The equation is in the paper.
+        :param unique_x: the array of unique x indices.
+        :param size: the size of the training dataset.
+        """
+
         scores, _ = self.neigh.kneighbors(self.x[unique_x], size)
         return -scores.sum(axis=1) / len(scores)
 
     def _check_densest(self, x_kneighbors, dense_scores, unique_x):
+        """Check if the sample x is the densest sample with its neighbors.
+        :param x_kneighbors: the array of neighbor indices.
+        :param dense_scores: the array of calculated dense scores.
+        :param unique_x: the array of unique x indices.
+        """
+
         maximum = dense_scores[np.where(unique_x == x_kneighbors[0])[0]]
         for idx in range(1, self.k):
             if dense_scores[np.where(unique_x == x_kneighbors[idx])[0]] > maximum:
@@ -40,6 +59,11 @@ class EGDIS(object):
         return True
 
     def fit(self, x, y):
+        """Run the algorithm and return selected samples.
+        :param x: the training set.
+        :param y: the labels of the training set.
+        """
+
         self.x = x
         self.y = y
         self.neigh.fit(x)
@@ -53,17 +77,13 @@ class EGDIS(object):
 
         # calculate the dense scores for unique and irrelevance_scores == 0 samples.
         size = len(self.x)
-        dense_scores = self._densest(unique_x, size)
+        dense_scores = self._dense(unique_x, size)
         selected = [self._check_densest(x_kneighbors, dense_scores, unique_x) for x_kneighbors in dense_x]
         selected = dense_x[selected]
-        selected = [x[0] for x in selected]
+        selected = selected[:, 0]
 
         # select from the boundary samples.
         selected_boundary = self.neigh_ind[irrelevance_scores >= int(self.k / 2)]
-        selected_boundary = [x[0] for x in selected_boundary]
+        selected_boundary = selected_boundary[:, 0]
 
-        return selected, selected_boundary
-
-    def reduce(self, percent):
-
-        return
+        return np.concatenate((selected, selected_boundary))
