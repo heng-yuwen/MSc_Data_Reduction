bdccdf17bae431924ba7a3759ca5a2c710eae85a
Yuwen Heng <hengyuwen_alex@163.com>
2020-07-07 19:02:24 +0800

    Experiment 4

--- cifar10_experiments.py
+++ cifar10_experiments.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 
 from lib.densenet import DenseNet121
-from lib.experiments import load_dataset, run_egdis
+from lib.experiments import load_dataset, run_cl
 
 x_train, x_valid, x_test, y_train, y_valid, y_test = load_dataset("cifar10")
 print("There are {} training samples and {} validation samples".format(x_train.shape[0], x_valid.shape[0]))
@@ -27,15 +27,15 @@ net = DenseNet121()
 # print("History saved.")
 
 # Experiment 3: train the EGDIS selected dataset
-history = run_egdis((x_train, y_train), (x_valid, y_valid), (x_test, y_test), net, "cifar10", 10, batch_size=batch_size)
-np.save(os.path.join(os.getcwd(), "models", "cifar10", "egdis_his.npy"), history)
-print("History saved.")
+# history = run_egdis((x_train, y_train), (x_valid, y_valid), (x_test, y_test), net, "cifar10", 10, batch_size=batch_size)
+# np.save(os.path.join(os.getcwd(), "models", "cifar10", "egdis_his.npy"), history)
+# print("History saved.")
 
 # Experiment 4: train the CL selected dataset
-# history = run_cl((x_train, y_train), (x_valid, y_valid), (x_test, y_test), net, "cifar10", 10, batch_size=batch_size)
-# for his in history:
-#     np.save(os.path.join(os.getcwd(), "models", "cifar10", "cl_his_size_" + str(his["size"]) + ".npy"), history)
-# print("History saved.")
+history = run_cl((x_train, y_train), (x_valid, y_valid), (x_test, y_test), net, "cifar10", 10, batch_size=batch_size)
+for his in history:
+    np.save(os.path.join(os.getcwd(), "models", "cifar10", "cl_his_size_" + str(his["size"]) + ".npy"), history)
+print("History saved.")
 
 # Experiment 5: train the WCL selected dataset
 # history = run_wcl((x_train, y_train), (x_valid, y_valid), (x_test, y_test), net, "cifar10", 10, batch_size=batch_size)
