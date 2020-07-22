5abc197fa9d367db2adb35e9ed79f4ef647d06fc
Yuwen Heng <hengyuwen@outlook.com>
2020-07-22 01:07:07 +0100

    update training his

--- cifar10_scripts/cifar10_experiments.sh
+++ cifar10_scripts/cifar10_experiments.sh
@@ -37,4 +37,4 @@ export DATASET_DIR=${TMP}/datasets/
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd ..
 
-python cifar10_experiments.py --experiment 7 --select 3 --batch_size 256  --stage 2
+python cifar10_experiments.py --experiment 2 --select 11 --batch_size 256  --stage 3
